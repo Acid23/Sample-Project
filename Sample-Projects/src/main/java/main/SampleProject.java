1b80a22f13d2985d35e9be82d96075e71ba58a99
hp <hp@DESKTOP-05OAIAK>
2019-03-01 14:03:33 +0530

    hi

--- Sample-Projects/src/main/java/main/SampleProject.java
+++ Sample-Projects/src/main/java/main/SampleProject.java
@@ -9,8 +9,7 @@ public class SampleProject {
 		return c;
 	}
 	public static int add(int a,int b) {
-		int c = 0;
-		c = a + b;
+		int c = a + b;
 		return c;
 	}
 	public static void main(String args[]) {
